60207544cb30e6acbf5909dd5a56d300b2d321f4
Witanday <witanday.cd@gmail.com>
2019-02-28 17:12:14 -0500

    j

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const express = require('express');
 const server = express();
+const helmet = require('helmet');
 const PORT  = process.env.PORT || 3111;
 
-server.get('/hello', (req,res)=>{
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req,res)=>{
 
     res.json({greetin:'Hello , it works'})
 })
